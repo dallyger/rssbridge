fccf3841980197ac97900d1501eecc5ccd0fe8f6
dallyger <v.neubauer@darlor.de>
2024-02-29 23:56:49 +0100

    init: prototype of a shopware plugin to rss feed converter

---
+++ rssbridge.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gocolly/colly"
+	"github.com/gorilla/feeds"
+)
+
+type Item struct {
+	Title string
+	Link string
+	Description string
+	Author string
+	Created string
+}
+
+func main() {
+	feed, err := shopware_store_plugin("swag136939272659f/shopware-6-sicherheits-plugin.html")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	json, err := feed.ToJSON()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	fmt.Println(json)
+}
+
+func shopware_store_plugin(id string) (*feeds.Feed, error) {
+	url := fmt.Sprintf("https://store.shopware.com/%s", id)
+	feed := &feeds.Feed{}
+
+	c := colly.NewCollector(
+		colly.AllowedDomains("store.shopware.com"),
+		// colly.CacheDir("./.cache/store.shopware.com"),
+		colly.UserAgent("dallyger/rssbridge"),
+	);
+	c.OnHTML("meta[name=\"author\"]", func(h *colly.HTMLElement) {
+		author := h.Attr("content")
+		if author != "" {
+			feed.Author = &feeds.Author{Name: author}
+		}
+	});
+	c.OnHTML("meta[itemprop=\"copyrightHolder\"]", func(h *colly.HTMLElement) {
+		feed.Copyright = h.Attr("content")
+	});
+	c.OnHTML("meta[property=\"og:title\"]", func(h *colly.HTMLElement) {
+		feed.Title = h.Attr("content")
+	});
+	c.OnHTML("meta[property=\"og:description\"]", func(h *colly.HTMLElement) {
+		feed.Description = h.Attr("content")
+	});
+	c.OnHTML("meta[property=\"og:url\"]", func(h *colly.HTMLElement) {
+		url = h.Attr("content")
+		feed.Link = &feeds.Link{Href: h.Attr("content")}
+	});
+	c.OnHTML("meta[property=\"og:image\"]", func(h *colly.HTMLElement) {
+		feed.Image = &feeds.Image{Link: h.Attr("content")}
+	});
+	c.OnHTML(".content--changelog h4", func(h *colly.HTMLElement) {
+		desc, err  :=  h.DOM.NextUntil("h4").Html()
+		if err != nil {
+			log.Fatal(err)
+		}
+		feed.Items = append(feed.Items, &feeds.Item{
+			Id: h.Text,
+			Title: h.Text,
+			Link: &feeds.Link{Href: url},
+			Description: desc,
+		})
+	});
+	c.Visit(url);
+
+	return feed, nil
+}
+
+// vim: noexpandtab
